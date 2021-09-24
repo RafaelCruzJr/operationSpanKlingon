b82764810ac7bbc82d83e3d44855bb10c5265250
Rafael Cruz <46229691+RafaelCruzJr@users.noreply.github.com>
2021-09-24 13:55:10 -0400

    Update klingMenu.js

--- klingMenu.js
+++ klingMenu.js
@@ -1,12 +1,25 @@
-var text = document.getElementById('opInstructions'), // get the instruction text element
-	nextButton = document.getElementById('next'),
-	previousButton = document.getElementById('previous'),
-	prac = document.getElementById('prac'),
-	op = document.getElementById('op'),
-	instructions = [
+var text = document.getElementById('opInstructions'), // grab instructions element
+	nextButton = document.getElementById('next'), // grab next button element
+	previousButton = document.getElementById('previous'), // grab previous button element
+	prac = document.getElementById('prac'), // grab practice button element
+	op = document.getElementById('op'), // grab operation span button element
+	cbAnswer = Number(opener.document.getElementById('cbAnswer').value),
+	numIns = 0;
+
+prac.addEventListener("click",function() {numIns++; text.innerHTML = instructions[numIns];}); // onClick function to continue after practice
+
+if (cbAnswer == 1) {
+	var A = "'A'",
+		B = "'L'";
+} else {
+	var A = "'L'",
+		B = "'A'";
+}
+
+var	instructions = [
 	"For this portion of the task, you will be performing a memory task at the same time as solving math equations.",
 	'A character will appear (ex: <img src = "Klingon/F.png" width = "30">), then a simple math equation (ex: [5/5] + 2 = 3), then another character, and another equation, and so on for anywhere from 3 to 9 cycles.',
-	"The math equations may be correct or incorrect. When an equation is shown to you, you must respond with either 'A' for correct or 'L' for not.",
+	`The math equations may be correct or incorrect. When an equation is shown to you, you must respond with either ${A} for correct or ${B} for not.`,
 	"Your job is to remember the characters and in which order they appeared, while accurately deciding whether the math equations are correct or not.",
 	"At the end of each set of cycles you will be asked to input in which order the characters appeared. There will be 7 sets of cycles (3 to 9) and you will do this twice for a total of 14 sets.",
 	"Try to be as accurate as possible with the equations, while remembering as many characters as possible.",
@@ -14,81 +27,42 @@ var text = document.getElementById('opInstructions'), // get the instruction tex
 	"Let's first do a short practice set of 5 letters to allow you to get used to the format. Press the button when you are ready.",
 	'You should have entered "1","2","3","4","5" for <img src = "Klingon/D.png" width = "30">,<img src = "Klingon/K.png" width = "30">,<img src = "Klingon/I.png" width = "30">,<img src = "Klingon/P.png" width = "30">,<img src = "Klingon/H.png" width = "30">, respectively.',
 	"Let's now move on to the main task. You will be expected to correctly answer at least 75% of the math equations within the time limit for the main task.",
-	"You will be given a break halfway through the task, during which your accuracy will be shown to you. Press the button when ready."],
-	numIns = 0;
-
-function changeText(newText) {
-	text.innerHTML = newText;
-} // make changing instruction text simple
-
-function displayPrevious() {
-	previousButton.style.display = 'block';
-} // make displaying previous button simple
-
-function removePrevious() {
-	previousButton.style.display = 'none';
-} // make removing previous button simple
-
-function displayNext() {
-	nextButton.style.display = 'block';
-} // make displaying next button simple
-
-function removeNext() {
-	nextButton.style.display = 'none';
-} // make removing next button simple
-
-function displayOp() {
-	op.style.display = 'block';
-} // make displaying next button simple
-
-function removeOp() {
-	op.style.display = 'none';
-} // make removing next button simple
-
-function displayPrac() {
-	prac.style.display = 'block';
-} // make displaying next button simple
-
-function removePrac() {
-	prac.style.display = 'none';
-} // make removing next button simple
-
-prac.addEventListener("click",function() {numIns++; changeText(instructions[numIns]);});
+	"You will be given a break halfway through the task, during which your accuracy will be shown to you. Press the button when ready."]; // series of instructions
 
 function next() {
 	numIns++;
 	if (numIns == 1) {
-		displayPrevious();
+		previousButton.style.display = 'block';
 	}
 	if (numIns == 7) {
-		removeNext();
-		displayPrac();
+		nextButton.style.display = 'none';
+		prac.style.display = 'block';
 	}
 	if (numIns == 9) {
-		displayPrevious();
+		previousButton.style.display = 'block';
 	}
 	if (numIns == 10) {
-		removeNext();
-		displayOp();
+		nextButton.style.display = 'none';
+		op.style.display = 'block';
 	}
-	changeText(instructions[numIns]); // display the current instruction text
+	text.innerHTML = instructions[numIns]; // display the current instruction text
 }
 
 function previous() {
 	numIns--;
 	if (numIns == 0) {
-		removePrevious();
+		previousButton.style.display = 'none';
 	}
 	if (numIns == 6) {
-		displayNext();
-		removePrac();
+		nextButton.style.display = 'block';
+		prac.style.display = 'none';
 	}
 	if (numIns == 8) {
-		removePrevious();
+		previousButton.style.display = 'none';
 	}
 	if (numIns == 9) {
-		displayNext();
-		removeOp();
+		nextButton.style.display = 'block';
+		op.style.display = 'none';
 	}
-	changeText(instructions[numIns]); // display the current instruction text
+	text.innerHTML = instructions[numIns]; // display the current instruction text
 }
